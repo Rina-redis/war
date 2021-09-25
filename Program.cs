1d647ac6aea73f70519fdb648662aef3d9f09487
rina redis <marinaredka1@gmail.com>
2021-09-25 18:45:28 +0300

    //

--- Program.cs
+++ Program.cs
@@ -4,16 +4,17 @@ using System.Collections.Generic;
 
 class Solution
 {
+    public static int rounds = 0;
     static void Main(string[] args)
-    {       
-        int rounds = 0;
+    {             
         int winner = 0;
         List<int> deck1 = new List<int>();
         List<int> deck2 = new List<int>();     
-        void IncreaseRoundsCount() => rounds++;
+        
 
         FillList(deck1);
         FillList(deck2);
+
         while (deck1.Count > 0 && deck2.Count > 0)
         {
            
@@ -40,7 +41,8 @@ class Solution
                 }
             }
         }
-        if (deck1.Count > deck2.Count)
+    
+        if (deck1.Count > deck2.Count)//also need PAT
         {
             winner = 1;
         }
@@ -48,66 +50,66 @@ class Solution
         {
             winner = 2;
         }
-        Console.WriteLine(winner +" "+ rounds);
+        Console.WriteLine(winner +" "+ rounds);     
+    }
 
 
-
-        (List<int>, List<int>) TryToWar(List<int> firstDeck, List<int> secondDeck)
+    public static void IncreaseRoundsCount() => rounds++;
+    public static (List<int>, List<int>) TryToWar(List<int> firstDeck, List<int> secondDeck)
+    {
+        if (firstDeck.Count > 4 && secondDeck.Count > 4)
         {
-            if (firstDeck.Count > 4 && secondDeck.Count > 4)
+            IncreaseRoundsCount();
+            int warCardA = firstDeck[4];
+            int warCardB = secondDeck[4];
+            if (warCardA > warCardB)
             {
-                IncreaseRoundsCount();
-                int warCardA = firstDeck[4];
-                int warCardB = secondDeck[4];
-                if (warCardA > warCardB)
+                for (int i = 0; i < 5; i++)
                 {
-                    for (int i = 0; i < 5; i++)
-                    {
-                        firstDeck.Add(secondDeck[0]);
-                        secondDeck.Remove(secondDeck[0]);
-                    }
+                    firstDeck.Add(secondDeck[0]);
+                    secondDeck.Remove(secondDeck[0]);
                 }
-                if (warCardB > warCardA)
-                {
-                    for (int i = 0; i < 5; i++)
-                    {
-                        secondDeck.Add(firstDeck[0]);
-                        firstDeck.Remove(firstDeck[0]);
-                    }
-                }
-                else
-                {
-                    TryToWar(firstDeck, secondDeck);
-                }
-            }          
-            return (firstDeck, secondDeck);
-        }
-        void FillList(List<int> deck)
-        {
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 0; i < n; i++)
-            {
-                string card = Console.ReadLine(); // the n cards of player 1  
-                string cardToAdd = DeleteSuit(card);
-                deck.Add(GetCardHash(cardToAdd));
             }
-
-        }
-        string DeleteSuit(string card)
-        {
-            return card.Remove(card.Length - 1);
-        }
-        int GetCardHash(string card)
-        {
-            string[] ranks = { "J", "Q", "K", "A" };
-            if (ranks.Contains(card))
+            if (warCardB > warCardA)
             {
-                return 11 + Array.IndexOf(ranks, card);
+                for (int i = 0; i < 5; i++)
+                {
+                    secondDeck.Add(firstDeck[0]);
+                    firstDeck.Remove(firstDeck[0]);
+                }
             }
             else
             {
-                return int.Parse(card);
+                TryToWar(firstDeck, secondDeck);//need to sth with cards from first war
             }
+        }
+        return (firstDeck, secondDeck);
+    }
+    public static void FillList(List<int> deck)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string card = Console.ReadLine(); // the n cards of player 1  
+            string cardToAdd = DeleteSuit(card);
+            deck.Add(GetCardHash(cardToAdd));
+        }
+
+    }
+    public static string DeleteSuit(string card)
+    {
+        return card.Remove(card.Length - 1);
+    }
+    public static int GetCardHash(string card)
+    {
+        string[] ranks = { "J", "Q", "K", "A" };
+        if (ranks.Contains(card))
+        {
+            return 11 + Array.IndexOf(ranks, card);
+        }
+        else
+        {
+            return int.Parse(card);
         }
     }
 }
